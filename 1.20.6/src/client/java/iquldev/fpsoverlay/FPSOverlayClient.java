81aad9c8060d2f1847d10bbf78b6cf624221c9f9
iquldev <iquldev@proton.me>
2024-07-14 13:24:15 +0300

    version 1.2

--- 1.20.6/src/client/java/iquldev/fpsoverlay/FPSOverlayClient.java
+++ 1.20.6/src/client/java/iquldev/fpsoverlay/FPSOverlayClient.java
@@ -18,57 +18,31 @@ public class FPSOverlayClient implements ClientModInitializer {
     private static final int UPDATE_INTERVAL = 30000;
     private static KeyBinding keyBinding;
     private boolean isF1Pressed = false;
-
-    boolean isShowed = FPSOverlayConfig.isShowed;
-    boolean isAdvancedShowed = FPSOverlayConfig.isAdvancedShowed;
 	@Override
 	public void onInitializeClient() {
-		MidnightConfig.init("fpsoverlay", FPSOverlayConfig.class);
-		HudRenderCallback.EVENT.register((context, tickDeltaManager) -> {
-			MinecraftClient client = MinecraftClient.getInstance();
+        MidnightConfig.init("fpsoverlay", FPSOverlayConfig.class);
+		HudRenderCallback.EVENT.register((context, tickDelta) -> {
+            boolean isShowed = FPSOverlayConfig.isShowed;
+            boolean isAdvancedShowed = FPSOverlayConfig.isAdvancedShowed;
 
-			int x = 10;
-			int y = 10;
+            MinecraftClient client = MinecraftClient.getInstance();
 
-			int padding = 5;
+            int x = 10;
+            int y = 10;
+            int padding = 5;
 
-			fps = client.getCurrentFps();
-			String text = fps + " FPS";
+            fps = client.getCurrentFps();
+            String text = fps + " FPS";
+            int backgroundColor = 0x80000000;
+            int textWidthFps = client.textRenderer.getWidth(text);
+            int textHeightFps = client.textRenderer.fontHeight;
 
-			int backgroundColor = 0x80000000;
+            if (isShowed && !isF1Pressed) {
+                context.fill(x - padding, y - padding, x + textWidthFps + padding, y + textHeightFps + padding, backgroundColor);
+                context.drawText(client.textRenderer, text, x, y, 0xFFFFFFFF, false);
+            }
 
-			int textWidthFps = client.textRenderer.getWidth(text);
-			int textHeightFps = client.textRenderer.fontHeight;
-
-			if (isShowed && !isF1Pressed) {
-				context.fill(x - padding, y - padding, x + textWidthFps + padding, y + textHeightFps + padding, backgroundColor);
-				context.drawText(client.textRenderer, text, x, y, 0xFFFFFFFF, false);
-
-				if (isAdvancedShowed && !isF1Pressed) {
-                    long currentTime = System.currentTimeMillis();
-
-                    if (fps < minFps) {
-                        minFps = fps;
-                    }
-                    if (fps > maxFps) {
-                        maxFps = fps;
-                    }
-
-                    if (currentTime - lastUpdateTime >= UPDATE_INTERVAL) {
-                        minFps = fps;
-                        maxFps = fps;
-                        lastUpdateTime = currentTime;
-                    }
-
-                    String minMaxText = minFps + " ▼ " + maxFps + " ▲";
-
-                    int advancedX = x + textWidthFps + 15;
-                    int textWidthMinMax = client.textRenderer.getWidth(minMaxText);
-
-                    context.fill(advancedX - padding, y - padding, advancedX + textWidthMinMax + padding, y + textHeightFps + padding, backgroundColor);
-                    context.drawText(client.textRenderer, minMaxText, advancedX, y, 0xFFFFFFFF, false);
-                }
-			} else if (isAdvancedShowed && !isF1Pressed) {
+            if (isAdvancedShowed && !isF1Pressed) {
                 long currentTime = System.currentTimeMillis();
 
                 if (fps < minFps) {
@@ -85,13 +59,13 @@ public class FPSOverlayClient implements ClientModInitializer {
                 }
 
                 String minMaxText = minFps + " ▼ " + maxFps + " ▲";
-
+                int advancedX = isShowed ? x + textWidthFps + 15 : x;
                 int textWidthMinMax = client.textRenderer.getWidth(minMaxText);
 
-                context.fill(x - padding, y - padding, x + textWidthMinMax + padding, y + textHeightFps + padding, backgroundColor);
-                context.drawText(client.textRenderer, minMaxText, x, y, 0xFFFFFFFF, false);
+                context.fill(advancedX - padding, y - padding, advancedX + textWidthMinMax + padding, y + textHeightFps + padding, backgroundColor);
+                context.drawText(client.textRenderer, minMaxText, advancedX, y, 0xFFFFFFFF, false);
             }
-		});
+        });
 
         keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "fpsoverlay.keys.show",
