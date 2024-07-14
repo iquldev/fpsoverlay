88cb2f47e6ae8ab6d8abc52f4c24adf59295ab01
iquldev <iquldev@proton.me>
2024-07-14 12:19:31 +0300

    version 1.2

--- src/client/java/iquldev/fpsoverlay/FPSOverlayConfig.java
+++
@@ -1,9 +0,0 @@
-package iquldev.fpsoverlay;
-
-import eu.midnightdust.lib.config.MidnightConfig;
-
-public class FPSOverlayConfig extends MidnightConfig {
-    @Comment(category = "text", centered = true) public static Comment display;
-    @Entry(category = "text") public static boolean isShowed = true;
-    @Entry(category = "text") public static boolean isAdvancedShowed = false;
-}
