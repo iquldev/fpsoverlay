e5cc823e329138e67942ce77582b491202833e67
iquldev <iquldev@proton.me>
2024-10-13 16:06:37 +0300

    version 1.5

--- 1.21/src/client/java/iquldev/fpsoverlay/FPSOverlayModMenu.java
+++ 1.21/src/client/java/iquldev/fpsoverlay/FPSOverlayModMenu.java
@@ -3,7 +3,6 @@ package iquldev.fpsoverlay;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import eu.midnightdust.lib.config.MidnightConfig;
-import net.minecraft.client.gui.screen.Screen;
 
 public class FPSOverlayModMenu implements ModMenuApi {
     @Override
--- 1.21/src/main/java/iquldev/fpsoverlay/FPSOverlay.java
+++ 1.21/src/main/java/iquldev/fpsoverlay/FPSOverlay.java
@@ -3,12 +3,14 @@ package iquldev.fpsoverlay;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import eu.midnightdust.lib.config.MidnightConfig;
 
 public class FPSOverlay implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("FPS Overlay");
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("FPS Overlay loaded!");
-	}
+    @Override
+    public void onInitialize() {
+        MidnightConfig.init("fpsoverlay", FPSOverlayConfig.class);
+        LOGGER.info("FPS Overlay loaded!");
+    }
 }
